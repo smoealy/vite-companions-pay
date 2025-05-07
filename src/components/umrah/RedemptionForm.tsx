f0b202f0b3130d604deebc3333378c25235a979e
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-07 05:33:57 -0400

    Update RedemptionForm.tsx

--- src/components/umrah/RedemptionForm.tsx
+++ src/components/umrah/RedemptionForm.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { useUser } from '@/contexts/UserContext';
+import { useUser } from "../../../contexts/UserContext";
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
