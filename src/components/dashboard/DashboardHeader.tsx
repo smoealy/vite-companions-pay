bfad29ba4d79573222e3c31e10f94864bfc03333
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-07 05:37:20 -0400

    Update DashboardHeader.tsx

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from '@/components/ui/button';
 import Logo from '@/components/ui-components/Logo';
 import { ConnectButton } from '@/walletConnect';
-import { useUser } from '@/contexts/UserContext';
+import { useUser } from "../../../contexts/UserContext";
 
 const DashboardHeader: React.FC = () => {
   const navigate = useNavigate();
