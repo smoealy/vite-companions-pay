8016906b50e4f34035f9182eff4d4278d967184e
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-07 23:12:42 -0400

    Update SavingsBalanceCard.tsx

--- src/components/web2/SavingsBalanceCard.tsx
+++ src/components/web2/SavingsBalanceCard.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { RefreshCcw, PlusCircle } from 'lucide-react';
 import { useToast } from '@/hooks/use-toast';
-import { useUser } from "../../../contexts/UserContext";
+import { useUser } from "@/contexts/UserContext";
 
 interface SavingsBalanceCardProps {
   earnedToday: number;
