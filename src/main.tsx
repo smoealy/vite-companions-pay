1fb9a26a1111f98ccc6b9a863102e70d4d64b7ae
smoealy <129239220+smoealy@users.noreply.github.com>
2025-05-07 22:58:25 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-
-import { createRoot } from 'react-dom/client'
-import { WalletProvider } from './walletConnect'
-import { UserProvider } from "./contexts/UserContext"
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { WalletProvider } from '@/walletConnect';
+import { UserProvider } from '@/contexts/UserContext';
+import App from './App';
+import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <UserProvider>
