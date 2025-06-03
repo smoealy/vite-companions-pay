d13322f817aa9262ae62b3d57713ba12424be3e3
smoealy <129239220+smoealy@users.noreply.github.com>
2025-06-03 15:47:38 -0400

    Update index.ts

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -2,4 +2,5 @@
 // Export all services from a central index file
 export * from './userService';
 export * from './redemptionService';
+export { db } from '@/firebaseConfig';
 export * from './activityService';
